dfcf8c95a3a3f5bf6414a502f181b3190e697444
Manikandan K <manikandan.kalaivanan@grey.com>
2022-07-07 13:32:03 +0530

    updated package.json and webpack.mix which gives version issue

---
+++ assets/working-js-files/custom.js
@@ -0,0 +1,11 @@
+require('./index'); // Initialized jquery
+/* call your 3rd party library 
+  from vendor 
+  like below 
+  
+  require('./vendor/jquery.ui.touch-punch.min');
+  */
+
+$(function () {
+  console.log("hello");
+});
